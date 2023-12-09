80feca28666c480283eb192268188cba7039d57a
tatsinda <blancleroytatsinda@gmail.com>
2023-12-09 22:10:17 +0100

    reparer

--- src/test/java/com/jenkinsTest/jenkinsTest/JenkinsTestApplicationTests.java
+++ src/test/java/com/jenkinsTest/jenkinsTest/JenkinsTestApplicationTests.java
@@ -5,17 +5,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.logging.Logger;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 @SpringBootTest
 class JenkinsTestApplicationTests {
 
 	public static Logger log= Logger.getLogger(JenkinsTestApplicationTests.class.getName());
 	@Test
-	public contextLoads() {
+	public void contextLoads() {
 		log.info("Text case execution.....");
-		log.info("Text case execution second log statement.....");
-		assertEquals(true,true);
+
 
 	}
 
